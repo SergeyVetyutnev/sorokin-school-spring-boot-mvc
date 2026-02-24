37145619222605da4d2f617051bf35d2d7dbf3e1
sergey_vetyutnev <sergey.vetyutnev@gmail.com>
2026-02-24 15:54:25 +0300

    init

---
+++ src/main/java/dev/vetyutnev/sorokinschoolspringbootmvc/SorokinSchoolSpringBootMvcApplication.java
@@ -0,0 +1,13 @@
+package dev.vetyutnev.sorokinschoolspringbootmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SorokinSchoolSpringBootMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SorokinSchoolSpringBootMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/vetyutnev/sorokinschoolspringbootmvc/SorokinSchoolSpringBootMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vetyutnev.sorokinschoolspringbootmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorokinSchoolSpringBootMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
